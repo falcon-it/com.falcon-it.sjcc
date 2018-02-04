fcfd5fde33554a53624fda62d2b1191600585acc
falcon-it <ilya.sokolov@live.ru>
2018-02-04 22:08:19 +0300

    +

--- src/packet/binary/BinaryReader.java
+++ src/packet/binary/BinaryReader.java
@@ -14,45 +14,42 @@ import packet.Reader;
  * @author Ilya Sokolow
  * @param <ReadObjectType>
  */
-public final class BinaryReader<ReadObjectType extends InputStream> implements Reader<ReadObjectType> {
-	private final Object m_SynchObject = new Object();
-	private DataInputStream m_DataInputStream = null;
-	private InputStreamDispatcher m_DataInputStreamDispatcher = null;
-	private ObjectInputStream m_ObjectInputStream = null;
-	private InputStreamDispatcher m_ObjectInputStreamDispatcher = null;
+public final class BinaryReader implements Reader<InputStream> {
+	private final DataInputStream m_DataStream;
+	private final InputStreamDispatcher m_StreamDispatcher;
+	
+	public BinaryReader() {
+		m_StreamDispatcher = new InputStreamDispatcher();
+		m_DataStream = new DataInputStream(m_StreamDispatcher);
+	}
 
 	/*
 	 * read byte array
 	 */
 	
 	private final void readByteArray(InputStream in, byte[] b, int off, int len) throws PacketIOException {
-		synchronized (m_SynchObject) {
-			if(m_DataInputStream == null) {
-				m_DataInputStreamDispatcher = new InputStreamDispatcher();
-				m_DataInputStream = new DataInputStream(m_DataInputStreamDispatcher);
+		try {
+			synchronized (m_DataStream) {
+				m_StreamDispatcher.putStream(in);
+				try {
+					m_DataStream.read(b, off, len);
+				}
+				finally {
+					m_StreamDispatcher.putStream(null);
+				}
 			}
-		}
-		
-		synchronized (m_DataInputStreamDispatcher) {
-			m_DataInputStreamDispatcher.putStream(in);
-			try {
-				m_DataInputStream.read(b, off, len);
-			} catch (IOException e) {
-				throw new PacketIOException(e);
-			}
-			finally {
-				m_DataInputStreamDispatcher.putStream(null);
-			}
+		} catch (IOException e) {
+			throw new PacketIOException(e);
 		}
 	}
 	
 	@Override
-	public void readBytes(ReadObjectType in, byte[] b) throws PacketIOException {
+	public void readBytes(InputStream in, byte[] b) throws PacketIOException {
 		readByteArray(in, b, 0, b.length);
 	}
 	
 	@Override
-	public void readBytes(ReadObjectType in, byte[] b, int off, int len) throws PacketIOException {
+	public void readBytes(InputStream in, byte[] b, int off, int len) throws PacketIOException {
 		readByteArray(in, b, off, len);
 	}
 	
@@ -65,95 +62,73 @@ public final class BinaryReader<ReadObjectType extends InputStream> implements R
 	}
 	
 	private final <T> T readData(readData<T> read, InputStream in) throws PacketIOException {
-		synchronized (m_SynchObject) {
-			if(m_DataInputStream == null) {
-				m_DataInputStreamDispatcher = new InputStreamDispatcher();
-				m_DataInputStream = new DataInputStream(m_DataInputStreamDispatcher);
+		try {
+			synchronized (m_DataStream) {
+				m_StreamDispatcher.putStream(in);
+				try {
+					return read.read(m_DataStream);
+				}
+				finally {
+					m_StreamDispatcher.putStream(null);
+				}
 			}
-		}
-		
-		synchronized (m_DataInputStreamDispatcher) {
-			m_DataInputStreamDispatcher.putStream(in);
-			try {
-				return read.read(m_DataInputStream);
-			} catch (IOException e) {
-				throw new PacketIOException(e);
-			}
-			finally {
-				m_DataInputStreamDispatcher.putStream(null);
-			}
+		} catch (IOException e) {
+			throw new PacketIOException(e);
 		}
 	}
 	
 	@Override
-	public boolean readBoolean(ReadObjectType in) throws PacketIOException {
+	public boolean readBoolean(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readBoolean(); }, in);
 	}
 	
 	@Override
-	public byte readByte(ReadObjectType in) throws PacketIOException {
+	public byte readByte(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readByte(); }, in);
 	}
 	
 	@Override
-	public char readChar(ReadObjectType in) throws PacketIOException {
+	public char readChar(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readChar(); }, in);
 	}
 	
 	@Override
-	public double readDouble(ReadObjectType in) throws PacketIOException {
+	public double readDouble(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readDouble(); }, in);
 	}
 	
 	@Override
-	public float readFloat(ReadObjectType in) throws PacketIOException {
+	public float readFloat(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readFloat(); }, in);
 	}
 	
 	@Override
-	public int readInt(ReadObjectType in) throws PacketIOException {
+	public int readInt(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readInt(); }, in);
 	}
 	
 	@Override
-	public long readLong(ReadObjectType in) throws PacketIOException {
+	public long readLong(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readLong(); }, in);
 	}
 	
 	@Override
-	public short readShort(ReadObjectType in) throws PacketIOException {
+	public short readShort(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readShort(); }, in);
 	}
 	
 	@Override
-	public String readString(ReadObjectType in) throws PacketIOException {
+	public String readString(InputStream in) throws PacketIOException {
 		return readData((DataInputStream data_in) -> { return data_in.readUTF(); }, in);
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
-	public <ObjectType> ObjectType readObject(ReadObjectType in) throws PacketIOException {
-		synchronized (m_SynchObject) {
-			if(m_ObjectInputStream == null) {
-				m_ObjectInputStreamDispatcher = new InputStreamDispatcher();
-				try {
-					m_ObjectInputStream = new ObjectInputStream(m_ObjectInputStreamDispatcher);
-				} catch (IOException e) {
-					throw new PacketIOException(e);
-				}
-			}
-		}
-		
-		synchronized (m_ObjectInputStreamDispatcher) {
-			m_ObjectInputStreamDispatcher.putStream(in);
-			try {
-				return (ObjectType)m_ObjectInputStream.readObject();
-			} catch (IOException|ClassNotFoundException e) {
-				throw new PacketIOException(e);
-			}
-			finally {
-				m_ObjectInputStreamDispatcher.putStream(null);
-			}
+	public <ObjectType> ObjectType readObject(InputStream in) throws PacketIOException {
+		try {
+			return (ObjectType)(new ObjectInputStream(in)).readObject();
+		} catch (IOException|ClassNotFoundException e) {
+			throw new PacketIOException(e);
 		}
 	}
 }
--- src/packet/binary/BinaryWriter.java
+++ src/packet/binary/BinaryWriter.java
@@ -14,45 +14,42 @@ import packet.Writer;
  * @author Ilya Sokolov
  * @param <WriteObjectType>
  */
-public final class BinaryWriter<WriteObjectType extends OutputStream> implements Writer<WriteObjectType> {
-	private final Object m_SynchObject = new Object();
-	private DataOutputStream m_DataOutputStream = null;
-	private OutputStreamDispatcher m_DataOutputStreamDispatcher = null;
-	private ObjectOutputStream m_ObjectOutputStream = null;
-	private OutputStreamDispatcher m_ObjectOutputStreamDispatcher = null;
-
+public final class BinaryWriter implements Writer<OutputStream> {
+	private final DataOutputStream m_DataStream;
+	private final OutputStreamDispatcher m_StreamDispatcher;
+	
+	public BinaryWriter() {
+		m_StreamDispatcher = new OutputStreamDispatcher();
+		m_DataStream = new DataOutputStream(m_StreamDispatcher);
+	}
+	
 	/*
 	 * write byte array
 	 */
 	
 	private final void writeByteArray(OutputStream out, byte[] b, int off, int len) throws PacketIOException {
-		synchronized (m_SynchObject) {
-			if(m_DataOutputStream == null) {
-				m_DataOutputStreamDispatcher = new OutputStreamDispatcher();
-				m_DataOutputStream = new DataOutputStream(m_DataOutputStreamDispatcher);
+		try {
+			synchronized (m_DataStream) {
+				m_StreamDispatcher.putStream(out);
+				try {
+					m_DataStream.write(b, off, len);
+				}
+				finally {
+					m_StreamDispatcher.putStream(null);
+				}
 			}
-		}
-		
-		synchronized (m_DataOutputStreamDispatcher) {
-			m_DataOutputStreamDispatcher.putStream(out);
-			try {
-				m_DataOutputStream.write(b, off, len);
-			} catch (IOException e) {
-				throw new PacketIOException(e);
-			}
-			finally {
-				m_DataOutputStreamDispatcher.putStream(null);
-			}
+		} catch (IOException e) {
+			throw new PacketIOException(e);
 		}
 	}
 	
 	@Override
-	public void write(WriteObjectType out, byte[] b) throws PacketIOException {
+	public void write(OutputStream out, byte[] b) throws PacketIOException {
 		writeByteArray(out, b, 0, b.length);
 	}
 	
 	@Override
-	public void write(WriteObjectType out, byte[] b, int off, int len) throws PacketIOException {
+	public void write(OutputStream out, byte[] b, int off, int len) throws PacketIOException {
 		writeByteArray(out, b, off, len);
 	}
 	
@@ -65,94 +62,72 @@ public final class BinaryWriter<WriteObjectType extends OutputStream> implements
 	}
 	
 	private final <T> void writeData(writeData<T> write, OutputStream out, T instance) throws PacketIOException {
-		synchronized (m_SynchObject) {
-			if(m_DataOutputStream == null) {
-				m_DataOutputStreamDispatcher = new OutputStreamDispatcher();
-				m_DataOutputStream = new DataOutputStream(m_DataOutputStreamDispatcher);
+		try {
+			synchronized (m_DataStream) {
+				m_StreamDispatcher.putStream(out);
+				try {
+					write.write(m_DataStream, instance);
+				}
+				finally {
+					m_StreamDispatcher.putStream(null);
+				}
 			}
-		}
-		
-		synchronized (m_DataOutputStreamDispatcher) {
-			m_DataOutputStreamDispatcher.putStream(out);
-			try {
-				write.write(m_DataOutputStream, instance);
-			} catch (IOException e) {
-				throw new PacketIOException(e);
-			}
-			finally {
-				m_DataOutputStreamDispatcher.putStream(null);
-			}
+		} catch (IOException e) {
+			throw new PacketIOException(e);
 		}
 	}
 	
 	@Override
-	public void writeBoolean(WriteObjectType out, boolean v) throws PacketIOException {
+	public void writeBoolean(OutputStream out, boolean v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Boolean data_v) -> { data_out.writeBoolean(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeByte(WriteObjectType out, byte v) throws PacketIOException {
+	public void writeByte(OutputStream out, byte v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Byte data_v) -> { data_out.writeByte(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeChar(WriteObjectType out, char v) throws PacketIOException {
+	public void writeChar(OutputStream out, char v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Character data_v) -> { data_out.writeChar(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeDouble(WriteObjectType out, double v) throws PacketIOException {
+	public void writeDouble(OutputStream out, double v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Double data_v) -> { data_out.writeDouble(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeFloat(WriteObjectType out, float v) throws PacketIOException {
+	public void writeFloat(OutputStream out, float v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Float data_v) -> { data_out.writeFloat(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeInt(WriteObjectType out, int v) throws PacketIOException {
+	public void writeInt(OutputStream out, int v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Integer data_v) -> { data_out.writeInt(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeLong(WriteObjectType out, long v) throws PacketIOException {
+	public void writeLong(OutputStream out, long v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Long data_v) -> { data_out.writeLong(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeShort(WriteObjectType out, short v) throws PacketIOException {
+	public void writeShort(OutputStream out, short v) throws PacketIOException {
 		writeData((DataOutputStream data_out, Short data_v) -> { data_out.writeShort(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeString(WriteObjectType out, String v) throws PacketIOException {
+	public void writeString(OutputStream out, String v) throws PacketIOException {
 		writeData((DataOutputStream data_out, String data_v) -> { data_out.writeUTF(data_v); }, out, v);
 	}
 	
 	@Override
-	public void writeObject(WriteObjectType out, Object v) throws PacketIOException {
-		synchronized (m_SynchObject) {
-			if(m_ObjectOutputStream == null) {
-				m_ObjectOutputStreamDispatcher = new OutputStreamDispatcher();
-				try {
-					m_ObjectOutputStream = new ObjectOutputStream(m_ObjectOutputStreamDispatcher);
-				} catch (IOException e) {
-					throw new PacketIOException(e);
-				}
-			}
-		}
-		
-		synchronized (m_ObjectOutputStreamDispatcher) {
-			m_ObjectOutputStreamDispatcher.putStream(out);
-			try {
-				m_ObjectOutputStream.writeObject(v);;
-			} catch (IOException e) {
-				throw new PacketIOException(e);
-			}
-			finally {
-				m_ObjectOutputStreamDispatcher.putStream(null);
-			}
+	public void writeObject(OutputStream out, Object v) throws PacketIOException {
+		try {
+			(new ObjectOutputStream(out)).writeObject(v);;
+		} catch (IOException e) {
+			throw new PacketIOException(e);
 		}
 	}
 }
